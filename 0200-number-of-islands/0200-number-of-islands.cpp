49f6bde20aa308643d8a98b0bb7f178739f88fd2
ManaliChan <manaligamer83@gmail.com>
2026-07-30 10:50:52 +0530

    Time: 33 ms (19.89%), Space: 18 MB (40.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,40 +1,67 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+    vector<int> parent;
 
-        if (i < 0 || j < 0 || i >= m || j >= n)
-            return;
+    int findPar(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = findPar(parent[x]);
+    }
 
-        if (grid[i][j] == '0')
-            return;
+    void unite(int x, int y) {
+        int px = findPar(x);
+        int py = findPar(y);
 
-        grid[i][j] = '0';
-
-        dfs(i - 1, j, grid); 
-        dfs(i + 1, j, grid);
-        dfs(i, j - 1, grid);
-        dfs(i, j + 1, grid);
+        if (px != py)
+            parent[px] = py;
     }
 
     int numIslands(vector<vector<char>>& grid) {
+
         int m = grid.size();
+        if (m == 0) return 0;
         int n = grid[0].size();
 
-        int count = 0;
+        parent.resize(m * n);
+
+        for (int i = 0; i < m * n; i++)
+            parent[i] = i;
+
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
 
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(i, j, grid);
-                }
+                if (grid[i][j] == '0')
+                    continue;
 
+                int idx1 = i * n + j;
+
+                for (int k = 0; k < 4; k++) {
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+
+                    if (ni >= 0 && ni < m &&
+                        nj >= 0 && nj < n &&
+                        grid[ni][nj] == '1') {
+
+                        int idx2 = ni * n + nj;
+                        unite(idx1, idx2);
+                    }
+                }
             }
         }
 
-        return count;
+        unordered_set<int> st;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1')
+                    st.insert(findPar(i * n + j));
+            }
+        }
+
+        return st.size();
     }
 };
